81108f63f426fac4933df85e9f2da33debb63903
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-12 16:38:26 -0700

    aa

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 /*
- * main - Prints all possible combinations of two two-digit numbers,
- * ranging from separated by a comma followed by a space.
- *
- * Return: Always 0
+ * main - Prints all possible combinations of two two-digit numbers.
+ *Return: Always 0
  */
 
 int main(void)
