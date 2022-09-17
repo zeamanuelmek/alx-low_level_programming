10aca9e0d3ba142cdff21922e1ce7f4e0e3e4143
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-16 17:44:15 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers ; check the code for main school students.
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0' + a);
+	}
+	_putchar('\n');
+}
