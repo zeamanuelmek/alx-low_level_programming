324d00aaa7906e0fb6f3dbbde4e28a210f22bf91
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-16 18:23:25 -0700

    sds

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - check the code.
+ * @n: the number of times to print _.
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
