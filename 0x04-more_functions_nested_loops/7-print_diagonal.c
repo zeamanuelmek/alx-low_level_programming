ff7420d24786a534ecfbfffe306200d3336adc23
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-16 18:29:16 -0700

    badmon

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "mian.h"
+/**
+ * print_diagonal - prints diagonal line.
+ * @n: number of times to print \
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j =1; j <= 1; j++)
+		{
+			if (i >= 2 && j <= i - 1)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\\');
+			}
+		}
+		if (i == n)
+		{
+			break;
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
