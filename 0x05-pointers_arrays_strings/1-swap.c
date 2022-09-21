2340ae0ad6fc0f393dd6f2650d17471317a4196f
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-20 18:25:42 -0700

    be real 100

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swapts the value of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
