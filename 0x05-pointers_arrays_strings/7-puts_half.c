8e69a844b41117e55c9116d8f1435562a3c290f5
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-21 04:55:04 -0700

    git

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *puts_half - prints half of a string, followed 
+ *puts_half - prints half of a string, followed
  *by a new line
  *@str: string to print
  */
