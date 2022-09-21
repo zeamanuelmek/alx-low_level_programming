37e9c592c18934ec6d13243ced3f3379629f40fa
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-20 20:27:00 -0700

    all

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,22 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strlen - retunrs the ength of a string
- * @s: string
- * Return: length
- */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-	return (len);
-}
-/**
  * puts_half - prints half of a string, followed by a new line
  * @str: string to print
  */
