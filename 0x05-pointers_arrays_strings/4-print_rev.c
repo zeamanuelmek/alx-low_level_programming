564327f5d974380a35502d530ca23b6855b0dc86
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-21 03:40:54 -0700

    now or never

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,17 +4,19 @@
  * print_rev - prints a string, in reverse, followed by a new line
  * @s: string to reverse
  */
+
 void print_dev(char *s)
 {
-int len = 0;
-int i;
 
-/*iterate to find length of string and point to last character*/
-while (*s != '\0')
-{
-	len++;
-	++s;
-}
+	int len = 0;
+	int i;
+
+	/*iterate to find length of string and point to last character*/
+	while (*s != '\0')
+	{
+		len++;
+		++s;
+	}
 
 /* go back to characher before null character */
 s--;
