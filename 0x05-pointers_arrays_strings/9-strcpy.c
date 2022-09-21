06b3130fa6f6de912f3a0fa1ef1beddade7e67b0
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-21 05:15:31 -0700

    horn

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -25,10 +25,11 @@ int _strlen(char *s)
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
-
-	for (i = 0; i <= _strlen(src); i++);
-		dest[i] = src[i];
-
+	while (*src)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
 	return (dest);
 }
