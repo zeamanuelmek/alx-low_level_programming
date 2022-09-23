fe7e30bee5677836f6891abde0c11ba70bebdf0a
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-22 18:21:24 -0700

    new

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,6 +5,16 @@
  * @src: string to append.
  * Return: concatenated string
  */
-char *strcat(char *dest, 
+char *strcat(char *dest, const char *src)
+{
+	int i = 0, dest_len = 0;
+
+	while (dest[i++])
+		dest_len++;
+
+	for (i = 0; src[i]; i++)
+		dest[dest_len++] = src[i];
+
 	return (dest);
+
 }
