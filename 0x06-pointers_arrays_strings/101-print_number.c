5c287e1f96fb7afe25c43a9879fcc0d234e585a5
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-23 12:56:06 -0700

    menged

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,15 +5,12 @@
  */
 void print_number(int n)
 {
-	unsigned int num = n;
-
 	if (n < 0)
 	{
-		putchar('-')
+		putchar('-');
 		num = -num;
 	}
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	putchar((num % 10) + '0');
+	if (n / 10)
+		print_number(n / 10);
+	putchar('\0' + n % 10);
 }
