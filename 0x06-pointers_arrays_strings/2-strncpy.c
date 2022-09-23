ace8c34dc5e6794777a27908682f512d73b4ab1d
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-22 19:41:58 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,12 +10,9 @@
  */
 char *strncpy(char *dest, char *src,int n)
 {
-	int i = 0, src_len = 0;
-
-	while (src[i++])
-		src_len++;
+	int i;
 	
-	for (i = 0; src[i] && i < n; i++)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 		dest[i] = src[i];
 
 	for (i = src_len; i < n; i++)
