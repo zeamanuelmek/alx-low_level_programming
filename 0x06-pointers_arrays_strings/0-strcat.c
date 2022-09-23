d30c1a84497e70a7d1de514664813d7004beb5e5
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-22 19:13:19 -0700

    mnw

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  *_strcat: concatenate two strings
  *@dest: string to append to
  @src: string to append
  * Return: concatenated string.
  */
-char *strcat(char *dest, const char *src)
+
+char *strcat(char *dest, char *src)
 {
 	int i = 0, n = 0;
 
