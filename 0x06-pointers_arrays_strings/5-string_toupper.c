1d2aeb30f342a6bcfc9995c9bdeef00e88b3e217
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-22 19:59:15 -0700

    some

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper: changes all lowercase letters tio
+ * uppercae
+ * @str: string
+ * Return: A pointer to the changed string.
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
