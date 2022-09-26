81786930196a3bfe17fb7f1d70915e50cca15b8c
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-26 12:18:35 -0700

    depressed

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,11 +13,11 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0' && s[i] != c) /* find match */
+	while (*(s + i) && *(s + i) != c) /* find match */
 		i++;
 
-	if (s[i] == c) /* if match, assign to address */
-		return (&s[i]);
+	if ((s + i) == c) /* if match, assign to address */
+		return (s + i);
 	else 
 		return (NULL);
 }
