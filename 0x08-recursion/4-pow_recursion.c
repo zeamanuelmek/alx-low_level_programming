c42664a4e28512a3c82a249f8d38850f5446eda1
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-28 18:51:49 -0700

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,7 +14,7 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (x == 0)
 		return (0);
-	if (x ==1 || y == 0)
+	if (x == 1 || y == 0)
 		return (1);
 
 	return (x * _pow_recursion(x, y - 1));
