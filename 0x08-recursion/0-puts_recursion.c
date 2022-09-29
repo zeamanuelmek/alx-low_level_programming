0654d2389bc07de3ab5f1f2d4d915755e67ba0f7
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-28 18:22:40 -0700

    m

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <unistd.h>
 #include <stdio.h>
+
 /**
- * _puts_recursion: print string with a new line.
- * @s: string to print.
+ *_puts_recursion: print string with a new line.
+ *@s: string to print.
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s)
