8d18db6f467341650c0ba6602e670c78739d45fb
zeamanuelmek <zeamekonnen@gmail.com>
2022-09-30 11:46:43 -0700

    new

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,4 +9,50 @@
  */
 
 bool is_num(char *argvv)
+{
+	int j = 0;
 
+	for (j = 0; argvv[j]; j++)
+	{
+		if (!(argvv[j] >= '0' && argvv[j] <= '9'))
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * main - print sum if all arguments given are numbers
+ * @argc: argument counter
+ * @argv: arguments
+ * Return: 0 on success, 1 if an argument was not a number
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int sum = 0;
+
+	/* validate input */
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	/** check al arguments to add numbers */
+	while (i < argc)
+	{
+		if (is_num(argv[i]))
+			sum += atoi(argv[i]);
+
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
+		}
+	printf("%d\n", sum);
+	
+	return (0);
+}
